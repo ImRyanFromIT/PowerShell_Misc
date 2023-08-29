995e4918581b5c8c6ec64a25b612244ceb30b131
Ryan Merkel <ryan.merkel@fhs-is.com>
2023-08-29 09:14:46 -0400

    Chocolatey script for Hugo

---
+++ IIS/IIS_BuildDependencies.bat
@@ -0,0 +1,14 @@
+@echo off
+echo Installing Chocolatey
+powershell Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+:: Section 2: Package Installs
+echo ========================================================================
+echo Installing Hugo
+choco install hugo --confirm
+echo ========================================================================
+echo Installing Go
+choco install golang -y
+echo ========================================================================
+echo Installing NodeJs
+choco install nodejs -y
+echo ========================================================================
