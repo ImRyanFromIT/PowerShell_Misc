cfd8d999fe2ed1ea451f0d5ce2dc225b8e9b18e2
Ryan Merkel~ <rgmerkel@gmail.com>
2023-08-29 10:12:13 -0400

    scripts

--- Git Actions/RunnerDownload.ps1
+++ Git Actions/RunnerDownload.ps1
@@ -1,4 +1,4 @@
-# Create a folder under the drive root
+# Create a folder under the drive root a
 mkdir actions-runner; cd actions-runner
 # Download the latest runner package
 Invoke-WebRequest -Uri https://github.com/actions/runner/releases/download/v2.308.0/actions-runner-win-x64-2.308.0.zip -OutFile actions-runner-win-x64-2.308.0.zip
