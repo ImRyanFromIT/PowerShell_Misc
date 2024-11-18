43eb1f3062dde6966649e8263604a389814667eb
ImRyanFromIT <117318573+ImRyanFromIT@users.noreply.github.com>
2024-11-17 23:20:42 -0500

    Add files via upload

---
+++ AD/ipdiscovery.ps1
@@ -0,0 +1,28 @@
+﻿
+$computers = Import-Csv -Path "path_to_your_file.csv"
+$results = @()
+foreach ($computer in $computers) {
+    $pingResult = Test-Connection -ComputerName $computer.hostname -Count 1 -ErrorAction SilentlyContinue
+    
+    if ($pingResult) {
+        $outputObject = [PSCustomObject]@{
+            hostname = $computer.hostname
+            IPAddress = $pingResult.IPV4Address.IPAddressToString
+            PSObject = $computer | Get-Member -MemberType NoteProperty | Where-Object {$_.Name -ne "hostname"} | ForEach-Object {
+                Add-Member -InputObject $outputObject -NotePropertyName $_.Name -NotePropertyValue $computer.$($_.Name) -PassThru
+            }
+        }
+    } else {
+        $outputObject = [PSCustomObject]@{
+            hostname = $computer.hostname
+            IPAddress = "No Response"
+            PSObject = $computer | Get-Member -MemberType NoteProperty | Where-Object {$_.Name -ne "hostname"} | ForEach-Object {
+                Add-Member -InputObject $outputObject -NotePropertyName $_.Name -NotePropertyValue $computer.$($_.Name) -PassThru
+            }
+        }
+    }
+    
+    $results += $outputObject
+}
+
+$results | Export-Csv -Path "results.csv" -NoTypeInformation
